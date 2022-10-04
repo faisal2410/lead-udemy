6dcf8d08f734d7577a1a69efcf19d160877944c4
Faisal Ahmed <faisal2410@yahoo.com>
2022-10-04 18:16:22 +0600

    initial commit

---
+++ controllers/auth.js
@@ -0,0 +1,160 @@
+const User=require("../models/user");
+const{ hashPassword, comparePassword } =require("../helpers/auth");
+const jwt =require("jsonwebtoken");
+const { uid } =require("uid");
+
+exports.register = async (req, res) => {
+  try {
+    // console.log(req.body);
+    const { name, email, password } = req.body;
+    // validation
+    if (!name) return res.status(400).send("Name is required");
+    if (!password || password.length < 6) {
+      return res
+        .status(400)
+        .send("Password is required and should be min 6 characters long");
+    }
+    let userExist = await User.findOne({ email }).exec();
+    if (userExist) return res.status(400).send("Email is taken");
+
+    // hash password
+    const hashedPassword = await hashPassword(password);
+
+    // register
+    const user = new User({
+      name,
+      email,
+      password: hashedPassword,
+    });
+    await user.save();
+    // console.log("saved user", user);
+    return res.json({ ok: true });
+  } catch (err) {
+    console.log(err);
+    return res.status(400).send("Error. Try again.");
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    // console.log(req.body);
+    const { email, password } = req.body;
+    // check if our db has user with that email
+    const user = await User.findOne({ email }).exec();
+    if (!user) return res.status(400).send("No user found");
+    // check password
+    const match = await comparePassword(password, user.password);
+    if (!match) return res.status(400).send("Wrong password");
+
+    // create signed jwt
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+    // return user and token to client, exclude hashed password
+    user.password = undefined;
+    // send token in cookie
+    res.cookie("token", token, {
+      httpOnly: true,
+      // secure: true, // only works on https
+    });
+    // send user as json response
+    res.json(user);
+  } catch (err) {
+    console.log(err);
+    return res.status(400).send("Error. Try again.");
+  }
+};
+
+exports.logout = async (req, res) => {
+  try {
+    res.clearCookie("token");
+    return res.json({ message: "Signout success" });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+exports.currentUser = async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id).select("-password").exec();
+    console.log("CURRENT_USER", user);
+    return res.json({ ok: true });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+exports.forgotPassword = async (req, res) => {
+  try {
+    const { email } = req.body;
+    // console.log(email);
+    const shortCode = uid(6).toUpperCase();
+    const user = await User.findOneAndUpdate(
+      { email },
+      { passwordResetCode: shortCode }
+    );
+    if (!user) return res.status(400).send("User not found");
+
+    // prepare for email
+    const params = {
+      Source: process.env.EMAIL_FROM,
+      Destination: {
+        ToAddresses: [email],
+      },
+      Message: {
+        Body: {
+          Html: {
+            Charset: "UTF-8",
+            Data: `
+                <html>
+                  <h1>Reset password</h1>
+                  <p>User this code to reset your password</p>
+                  <h2 style="color:red;">${shortCode}</h2>
+                  <i>edemy.com</i>
+                </html>
+              `,
+          },
+        },
+        Subject: {
+          Charset: "UTF-8",
+          Data: "Reset Password",
+        },
+      },
+    };
+
+    const emailSent = SES.sendEmail(params).promise();
+    emailSent
+      .then((data) => {
+        console.log(data);
+        res.json({ ok: true });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+exports.resetPassword = async (req, res) => {
+  try {
+    const { email, code, newPassword } = req.body;
+    // console.table({ email, code, newPassword });
+    const hashedPassword = await hashPassword(newPassword);
+
+    const user = User.findOneAndUpdate(
+      {
+        email,
+        passwordResetCode: code,
+      },
+      {
+        password: hashedPassword,
+        passwordResetCode: "",
+      }
+    ).exec();
+    res.json({ ok: true });
+  } catch (err) {
+    console.log(err);
+    return res.status(400).send("Error! Try again.");
+  }
+};
---
+++ routes/auth.js
@@ -0,0 +1,30 @@
+const express =require("express");
+
+const router = express.Router();
+
+// middleware
+const { requireSignin } =require("../middlewares");
+
+// controllers
+const {
+  register,
+  login,
+  logout,
+  currentUser,
+  forgotPassword,
+  resetPassword,
+} =require("../controllers/auth");
+
+router.post("/register", register);
+router.post("/login", login);
+router.get("/logout", logout);
+router.get("/current-user", requireSignin, currentUser);
+router.post("/forgot-password", forgotPassword);
+router.post("/reset-password", resetPassword);
+router.get("/",(req,res)=>{
+  res.status(200).json({
+    message:"<========== Welcome to lead educare ========>"
+  })
+})
+
+module.exports = router;
