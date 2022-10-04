13fd6910947cec9b524f44ef65261bed1a2bf48b
Faisal Ahmed <faisal2410@yahoo.com>
2022-10-04 19:21:41 +0600

    user model updated

--- models/user.js
+++ models/user.js
@@ -1,43 +1,123 @@
+const crypto = require("crypto");
 const mongoose =require("mongoose");
+const bcrypt = require("bcryptjs");
+const validator = require("validator");
+
 const { Schema } = mongoose;
 const { ObjectId } = Schema;
 
 const userSchema = new Schema(
   {
-    name: {
-      type: String,
-      trim: true,
-      required: true,
-    },
     email: {
       type: String,
+      validate: [validator.isEmail, "Provide a valid Email"],
       trim: true,
-      required: true,
+      lowercase: true,
       unique: true,
+      required: [true, "Email address is required"],
     },
     password: {
       type: String,
-      required: true,
-      min: 6,
-      max: 64,
+      required: [true, "Password is required"],
+      validate: {
+        validator: (value) =>
+          validator.isStrongPassword(value, {
+            minLength: 6,
+            minLowercase: 3,
+            minNumbers: 1,
+            minUppercase: 1,
+            minSymbols: 1,
+          }),
+        message: "Password {VALUE} is not strong enough.",
+      },
     },
-    picture: {
+    confirmPassword: {
       type: String,
-      default: "/avatar.png",
+      required: [true, "Please confirm your password"],
+      validate: {
+        validator: function (value) {
+          return value === this.password;
+        },
+        message: "Passwords don't match!",
+      },
     },
     role: {
-      type: [String],
-      default: ["Subscriber"],
-      enum: ["Subscriber", "Instructor", "Admin"],
-    },    
-    passwordResetCode: {
-      data: String,
-      default: "",
+      type: String,
+      enum: ["subscriber", "instructor", "admin"],
+      default: "subscriber",
+    },        
+    firstName: {
+      type: String,
+      required: [true, "Please provide a first name"],
+      trim: true,
+      minLength: [3, "Name must be at least 3 characters."],
+      maxLength: [100, "Name is too large"],
     },
+    lastName: {
+      type: String,
+      required: [true, "Please provide a first name"],
+      trim: true,
+      minLength: [3, "Name must be at least 3 characters."],
+      maxLength: [100, "Name is too large"],
+    },
+    contactNumber: {
+      type: String,
+      validate: [validator.isMobilePhone, "Please provide a valid contact number"],
+    },   
+
+    imageURL: {
+      type: String,
+      validate: [validator.isURL, "Please provide a valid url"],
+    },
+    status: {
+      type: String,
+      default: "inactive",
+      enum: ["active", "inactive", "blocked"],
+    },
+
+    confirmationToken: String,
+    confirmationTokenExpires: Date,
+    passwordChangedAt: Date,
+    passwordResetToken: String,
+    passwordResetExpires: Date,   
     courses: [{ type: ObjectId, ref: "Course" }],
   },
   { timestamps: true }
 );
+
+userSchema.pre("save", function (next) {
+  if (!this.isModified("password")) {
+    //  only run if password is modified, otherwise it will change every time we save the user!
+    return next();
+  }
+  const password = this.password;
+
+  const hashedPassword = bcrypt.hashSync(password);
+
+  this.password = hashedPassword;
+  this.confirmPassword = undefined;
+
+  next();
+});
+
+userSchema.methods.comparePassword = function (password, hash) {
+  const isPasswordValid = bcrypt.compareSync(password, hash);
+  return isPasswordValid;
+};
+
+userSchema.methods.generateConfirmationToken = function () {
+  const token = crypto.randomBytes(32).toString("hex");
+
+  this.confirmationToken = token;
+
+  const date = new Date();
+
+  date.setDate(date.getDate() + 1);
+  this.confirmationTokenExpires = date;
+
+  return token;
+};
+
 const User = mongoose.model("User", userSchema);
 
 module.exports = User;
