67002ebd0901289d9da563638ebaa8f189be1d46
Faisal Ahmed <faisal2410@yahoo.com>
2022-10-09 15:46:46 +0600

    forget and reset password added

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,10 @@
+const User=require("../models/user");
 const { signupService, findUserByEmail,findUserById,forgotPassword,resetPassword, findUserByToken } = require("../services/auth");
 const { generateToken } = require("../helpers/auth");
+const nanoid = require("nanoid");
+// sendgrid
+const sgMail = require("@sendgrid/mail");
+sgMail.setApiKey(process.env.SENDGRID_KEY);
 
 
 exports.register = async (req, res) => {
@@ -110,64 +115,58 @@ exports.currentUser = async (req, res) => {
 };
 
 exports.forgotPassword = async (req, res) => {
+  const { email } = req.body;
+  // find user by email
+  const user = await findUserByEmail(email);
+  console.log("USER ===> ", user);
+  if (!user) {
+    return res.json({ error: "User not found" });
+  }
+  // generate code
+  const resetCode = nanoid(5).toUpperCase();  
+  // save to db
+  user.resetCode = resetCode;
+   user.save({ validateBeforeSave: false });
+  // prepare email
+  const emailData = {
+    from: process.env.EMAIL_FROM,
+    to: user.email,
+    subject: "Password reset code",
+    html: `<h1>Your password  reset code is: ${resetCode}</h1>`,
+  };
+  // send email
   try {
-    const { email } = req.body;
-    // console.log(email);
-    const shortCode = uid(6).toUpperCase();
-    const user = await forgotPassword(email,shortCode)
-    if (!user) return res.status(400).send("User not found");
-
-    // prepare for email
-    const params = {
-      Source: process.env.EMAIL_FROM,
-      Destination: {
-        ToAddresses: [email],
-      },
-      Message: {
-        Body: {
-          Html: {
-            Charset: "UTF-8",
-            Data: `
-                <html>
-                  <h1>Reset password</h1>
-                  <p>User this code to reset your password</p>
-                  <h2 style="color:red;">${shortCode}</h2>
-                  <i>leadeducare.com</i>
-                </html>
-              `,
-          },
-        },
-        Subject: {
-          Charset: "UTF-8",
-          Data: "Reset Password",
-        },
-      },
-    };
-
-    const emailSent = SES.sendEmail(params).promise();
-    emailSent
-      .then((data) => {
-        console.log(data);
-        res.json({ ok: true });
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    const data = await sgMail.send(emailData);
+    console.log(data);
+    res.json({ ok: true });
   } catch (err) {
     console.log(err);
+    res.json({ ok: false });
   }
 };
 
 exports.resetPassword = async (req, res) => {
   try {
-    const { email, code, newPassword } = req.body;
-    // console.table({ email, code, newPassword });
-    // const hashedPassword = await hashPassword(newPassword);
-
-    const user = await resetPassword(email,code,newPassword)
-    res.json({ ok: true });
+    const { email, password, resetCode } = req.body;
+    // find user based on email and resetCode
+    const user = await User.findOne({ email, resetCode });
+    // if user not found
+    if (!user) {
+      return res.json({ error: "Email or reset code is invalid" });
+    }
+    // if password is short
+    // if (!password || password.length < 6) {
+    //   return res.json({
+    //     error: "Password is required and should be 6 characters long",
+    //   });
+    // }
+    // // hash password
+    // const hashedPassword = await hashPassword(password);
+    // user.password = hashedPassword;
+    user.resetCode = "";
+    user.save({ validateBeforeSave: false });
+    return res.json({ ok: true });
   } catch (err) {
     console.log(err);
-    return res.status(400).send("Error! Try again.");
   }
 };
